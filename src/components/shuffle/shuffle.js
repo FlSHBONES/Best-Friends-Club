8fa7036ea681cc9f4356d39c30298f7b6ef0e96a
Tim Fisher <tgfisher26@gmail.com>
2019-01-14 23:08:21 -0500

    retrofitted black jack into standard react application

---
+++ src/components/shuffle/shuffle.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import "./wrapper.css";
+
+function shuffle(a) {
+  let k,
+    t,
+    j,
+    i = a.length,
+    rand = Math.random;
+
+  // For each element in the array, swap it with a random
+  // element (which might be itself)
+  while (i--) {
+    k = (rand() * (i + 1)) | 0;
+    t = a[k];
+    a[k] = a[i];
+    a[i] = t;
+  }
+  return a;
+}
+
+export default shuffle;
